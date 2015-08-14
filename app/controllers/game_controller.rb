4a29908eb616ca40919ffb1472943d97b2cb3e3f
LightStrawberry <l21828604@gmail.com>
2015-08-14 11:45:13 -0700

    add Basic css and js

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -11,10 +11,31 @@ class GameController < ApplicationController
 		@game = Game.find(params[:id])
 	end
 
+	def edit
+		@game = Game.find(params[:id])
+	end
+
 	def create
 		@games = Game.new(games_params)
 		@games.save
 		redirect_to @games
+	end
+
+	def update
+	    @game = Game.find(params[:id])
+	 
+	    if @game.update(games_params)
+	      redirect_to @game
+	    else
+	      render 'edit'
+	    end
+	end
+
+	def destroy
+		@game =  Game.find(params[:id])
+		@game.destroy
+
+		redirect_to game_index_path
 	end
 
 	private
